1651af398c46e994df4179cfa41bf80cb025e7e0
zgldh <zgldh@hotmail.com>
2017-07-12 22:34:41 +0800

    Update QiniuFilesystemServiceProvider.php
    Fix missing key

--- src/QiniuFilesystemServiceProvider.php
+++ src/QiniuFilesystemServiceProvider.php
@@ -40,8 +40,8 @@ class QiniuFilesystemServiceProvider extends ServiceProvider
                     $config['secret_key'],
                     $config['bucket'],
                     $domains,
-                    $config['notify_url'] ? $config['notify_url'] : null,
-                    $config['access'] ? $config['access'] : 'public'
+                    isset($config['notify_url']) ? $config['notify_url'] : null,
+                    isset($config['access']) ? $config['access'] : 'public'
                 );
                 $file_system = new Filesystem($qiniu_adapter);
                 $file_system->addPlugin(new PrivateDownloadUrl());
